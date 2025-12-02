f67c1096ca509476c54a15601fe1659599087102
Rakesh Ramanjulu <rraman3110@gmail.com>
2025-12-02 01:06:00 -0500

    Update ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -12,7 +12,7 @@ Script Purpose:
 if OBJECT_ID('bronze.crm_cust_info', 'U') is not null
 	drop table bronze.crm_cust_info
 create table bronze.crm_cust_info(
-	cst_id int PRIMARY KEY, 
+	cst_id int, 
 	cst_key nvarchar(50),
 	cst_firstname nvarchar(50), 
 	cst_lastname nvarchar(50), 
@@ -25,7 +25,7 @@ GO
 if OBJECT_ID('bronze.crm_prd_info', 'U') is not null
 	drop table bronze.crm_prd_info
 create table bronze.crm_prd_info(
-	prd_id int PRIMARY KEY, 
+	prd_id int, 
 	prd_key nvarchar(255), 
 	prd_nm nvarchar(255), 
 	prd_cost int, 
@@ -38,7 +38,7 @@ GO
 if OBJECT_ID('bronze.crm_sales_details', 'U') is not null
 	drop table bronze.crm_sales_details
 create table bronze.crm_sales_details(
-	sls_ord_num nvarchar(50) PRIMARY KEY,
+	sls_ord_num nvarchar(50),
 	sls_prd_key nvarchar(50), 
 	sls_cust_id int, 
 	sls_order_dt int, 
@@ -53,7 +53,7 @@ GO
 if OBJECT_ID('bronze.erp_cust_az12', 'U') is not null
 	drop table bronze.erp_cust_az12
 create table bronze.erp_cust_az12(
-    cid    NVARCHAR(50) PRIMARY KEY,
+    cid    NVARCHAR(50),
     bdate  DATE,
     gen    NVARCHAR(50)
 );
@@ -62,7 +62,7 @@ GO
 if OBJECT_ID('bronze.erp_loc_a101', 'U') is not null
 	drop table bronze.erp_loc_a101
 create table bronze.erp_loc_a101(
-    cid    NVARCHAR(50) PRIMARY KEY,
+    cid    NVARCHAR(50),
     cntry NVARCHAR(50)
 );
 GO
@@ -70,7 +70,7 @@ GO
 if OBJECT_ID('bronze.erp_px_cat_g1v2', 'U') is not null
 	drop table bronze.erp_px_cat_g1v2
 create table bronze.erp_px_cat_g1v2(
-	id nvarchar(50) PRIMARY KEY, 
+	id nvarchar(50), 
 	cat nvarchar(50), 
 	subcat nvarchar(50), 
 	maintenance nvarchar(50)
