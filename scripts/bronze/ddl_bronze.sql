84857499648c281a6890f94f3bd2dcff626d761c
Rakesh Ramanjulu <rraman3110@gmail.com>
2025-12-02 00:28:52 -0500

    Define DDL structure for bronze tables

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -8,3 +8,71 @@ Script Purpose:
 	  Run this script to re-define the DDL structure of 'bronze' Tables
 ===============================================================================
 */
+
+if OBJECT_ID('bronze.crm_cust_info', 'U') is not null
+	drop table bronze.crm_cust_info
+create table bronze.crm_cust_info(
+	cst_id int PRIMARY KEY, 
+	cst_key nvarchar(50),
+	cst_firstname nvarchar(50), 
+	cst_lastname nvarchar(50), 
+	cst_marital_status nvarchar(10), 
+	cst_gender nvarchar(10), 
+	cst_create_date DATE
+);
+GO
+
+if OBJECT_ID('bronze.crm_prd_info', 'U') is not null
+	drop table bronze.crm_prd_info
+create table bronze.crm_prd_info(
+	prd_id int PRIMARY KEY, 
+	prd_key nvarchar(255), 
+	prd_nm nvarchar(255), 
+	prd_cost int, 
+	prd_line nvarchar(10), 
+	prd_start_dt DATE, 
+	prd_end_dt DATE
+);
+GO
+
+if OBJECT_ID('bronze.crm_sales_details', 'U') is not null
+	drop table bronze.crm_sales_details
+create table bronze.crm_sales_details(
+	sls_ord_num nvarchar(50) PRIMARY KEY,
+	sls_prd_key nvarchar(50), 
+	sls_cust_id int, 
+	sls_order_dt int, 
+	sls_ship_dt  INT,
+    sls_due_dt   INT,
+    sls_sales    INT,
+    sls_quantity INT,
+    sls_price    INT
+);
+GO
+
+if OBJECT_ID('bronze.erp_cust_az12', 'U') is not null
+	drop table bronze.erp_cust_az12
+create table bronze.erp_cust_az12(
+    cid    NVARCHAR(50) PRIMARY KEY,
+    bdate  DATE,
+    gen    NVARCHAR(50)
+);
+GO
+
+if OBJECT_ID('bronze.erp_loc_a101', 'U') is not null
+	drop table bronze.erp_loc_a101
+create table bronze.erp_loc_a101(
+    cid    NVARCHAR(50) PRIMARY KEY,
+    cntry NVARCHAR(50)
+);
+GO
+
+if OBJECT_ID('bronze.erp_px_cat_g1v2', 'U') is not null
+	drop table bronze.erp_px_cat_g1v2
+create table bronze.erp_px_cat_g1v2(
+	id nvarchar(50) PRIMARY KEY, 
+	cat nvarchar(50), 
+	subcat nvarchar(50), 
+	maintenance nvarchar(50)
+);
+GO
